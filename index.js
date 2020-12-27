ffeccfcdf3a1bd8672d212f292725fb920328c83
Ararat <ararat.martirossyan@jumbo.com>
2020-12-27 17:08:44 +0100

    Added Dockerfile and Docker Config

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-require('dotenv').config()
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const http = require("http");
-const cors = require("cors");
-const { routes } = require("./src/routes");
-const checkJWTSign = require('./src/middlewares/jwtCheck.middleware')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const http = require('http');
+const cors = require('cors');
+const { routes } = require('./src/routes');
+const { join } = require('path');
+
 // настроим подключение к бд
-mongoose.connect(process.env.MONGO_URI, {
+const mongoHost = process.env.MONGO_HOST;
+const mongoPort = process.env.MONGO_PORT;
+const mongoDbname = process.env.DB_NAME;
+
+mongoose.connect(`mongodb://${mongoHost}:${mongoPort}/${mongoDbname}`, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,12 +26,14 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// app.use('/', (_, res) => res.sendFile(join(__dirname + '/index.html')));
+
 routes.forEach((item) => {
   app.use(`/api/v1/${item}`, require(`./src/routes/${item}`));
 });
 
 // объявим наши  роуты
-const PORT = 1710;
+const PORT = process.env.PORT;
 http.createServer({}, app).listen(PORT);
 
 console.log(`Server running at ${PORT}`);
